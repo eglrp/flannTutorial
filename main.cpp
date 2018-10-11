f87f6bbc7aa57d5a18631be5b89c7aa5e76546a6
Yousof <yousofebneddin@yahoo.com>
2018-10-11 13:28:07 -0700

    working example of pangolin

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <random>
 
+#include <pangolin/pangolin.h>
 
 typedef std::vector<std::vector<float>> stdMat;
 
@@ -40,7 +41,37 @@ int main(int argc, char** argv)
   int numPoints = 1000;
   int dim = 3;
   int maxRange = 100;
-  generateRandomStdMat(numPoints,dim,maxRange);
+  stdMat vecs = generateRandomStdMat(numPoints,dim,maxRange);
+  printVecs(vecs);
 
+  pangolin::CreateWindowAndBind("Main",640,480);
+  glEnable(GL_DEPTH_TEST);
+
+  // Define Projection and initial ModelView matrix
+
+  pangolin::OpenGlRenderState s_cam(
+		  pangolin::ProjectionMatrix(640,480,420,420,320,240,0.2,100),
+		  pangolin::ModelViewLookAt(-2,2,-2, 0,0,0, pangolin::AxisY)
+		  );
+
+ // Create Interactive View in window
+ 
+  pangolin::Handler3D handler(s_cam);
+  pangolin::View& d_cam = pangolin::CreateDisplay()
+	  .SetBounds(0.0, 1.0, 0.0, 1.0, -640.0f/480.0f)
+	  .SetHandler(&handler);
+
+  while( !pangolin::ShouldQuit() )
+  {
+    // Clear screen and activate view to render into
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    d_cam.Activate(s_cam);
+
+    // Render OpenGL Cube
+    pangolin::glDrawColouredCube();
+
+    // Swap frames and Process Events
+    pangolin::FinishFrame();
+  }
   return 0;
 }
--- visuallizer.hpp
+++ visuallizer.hpp
@@ -1,22 +1,29 @@
 #ifndef VISUALLIZER_HPP
 #define VISUALLIZER_HPP
 
-//#include <pcl/visualization/cloud_viewer.h>
-//#include <vector>
+#include "pangolin/pangolin.h"
 
-//template<typename T>
-//class Vis3D{
-//private:
-//  //mglData m_data;
+template<typename T>
+class Vis3D{
+private:
 
-//public:
-//  Vis3D(){
+public:
+  Vis3D(){
+    pangolin::CreateWindowAndBind("Main",640,480);
+		glEnable(GL_DEPTH_TEST);
 
-//  }
+		pangolin::OpenGlRenderState s_cam(
+										pangolin::ProjectionMatrix(640,480,420,420,320,240,0.2,100),
+										pangolin::ModelViewLookAt(-2,2,-2,0,0,0, pangolin::AxisY)
+										);
 
-//  void plotPoints(std::vector<std::vector<float>>& mat){
+		// Create Interactive View in window 
+		pangolin::Handler3D handler(s_cam);
+  }
 
-//  }
-//};
+  void plotPoints(std::vector<std::vector<float>>& mat){
+
+  }
+};
 
 #endif // VISUALLIZER_HPP
